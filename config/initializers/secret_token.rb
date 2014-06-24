ed1a910dba8ae291b0046438b0db2b70f853f1e8
dbushkanets <dbushkanets@harryfox.com>
2014-06-23 21:36:35 -0400

    changed to postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubDiscovery::Application.config.secret_key_base = 'e6f6b85ffd0b6beff8efe5b1492590733552a96bf0b08a8300cfe79b7fcba27bd799a912a8509bc74aadf3319d7e1b392f480d6bdcc9add5b72a35aaccbeac2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GithubDiscovery::Application.config.session_store :cookie_store, key: '_github_discovery_session'
